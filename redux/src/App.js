b34219c5f3bc5bb8af73e186b32fb592fe65aee5
Reshma-21 <143719876+Reshma-21@users.noreply.github.com>
2024-05-13 14:54:23 +0530

    Update App.js

--- redux/src/App.js
+++ redux/src/App.js
@@ -25,20 +25,7 @@ function App() {
   return (
     <div className="App">
       <h1>REDUX</h1>
-      <div>
-      <input ref={inputRef}></input>
-      </div>
-      <br/>
-      <div>
-    <button onClick={()=>{
-      dispatch({type:"addtollywoodActor",data:inputRef.current.value});
-    }}>TollywoodActors</button>
-    <button onClick={()=>{
-      dispatch({type:"addbollywoodActors",data:inputRef.current.value});
-    }}>bollywoodActors</button>
-    </div>
-    <h2>Tollywood Actors:{storeObj.tollywoodActors.join()}</h2>
-    <h2>Bollwood Actors:{storeObj.bollywoodActors.join()}</h2>
+     <h1>changes made</h1>
     </div>
   );
 }
